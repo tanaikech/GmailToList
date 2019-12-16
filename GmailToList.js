63b8ccc58ac94c55b2fa250dcb902d82691c3f73
tanaikech <tanaike@hotmail.com>
2019-12-16 15:56:52 +0900

    1st commit

---
+++ GmailToList.js
@@ -0,0 +1,274 @@
+/**
+ * GitHub  https://github.com/tanaikech/GmailToList<br>
+ * Get all messages from Gmail as a list.<br>
+ * @param {Object} Object Object
+ * @return {Object} Return Object
+ */
+function Do(object) {
+    return new GmailToList(object).Do();
+}
+;
+(function(r) {
+  var GmailToList;
+  GmailToList = (function() {
+    var convertObjectToArray, getAllMessagesAsObject, getAllMessagesByGmailAPI, getData, getMessages;
+
+    GmailToList.name = "GmailToList";
+
+    function GmailToList(obj_) {
+      this.cellLmit = 50000;
+      this.url = "https://www.googleapis.com/gmail/v1/users/";
+      if (!obj_) {
+        obj_ = {};
+      }
+      if (!("userId" in obj_)) {
+        obj_.userId = "me";
+      }
+      if (!("exportAsObject" in obj_)) {
+        obj_.exportAsObject = false;
+      }
+      if (!("headers" in obj_)) {
+        obj_.headers = true;
+      }
+      this.obj = obj_;
+      this.obj.statistics = {
+        totalThreads: 0,
+        totalMessages: 0,
+        totalMessageSize: 0,
+        totalAttachmentFiles: 0,
+        totalAttachmentFileSize: 0
+      };
+    }
+
+    GmailToList.prototype.Do = function() {
+      var array, err, obj, resObj;
+      try {
+        obj = getAllMessagesAsObject.call(this);
+        array = convertObjectToArray.call(this, obj);
+        resObj = {
+          statistics: this.obj.statistics
+        };
+        if (this.obj.exportAsObject) {
+          resObj.object = obj;
+          return resObj;
+        }
+        resObj.array = array;
+        return resObj;
+      } catch (error) {
+        err = error;
+        return {
+          error: err
+        };
+      }
+    };
+
+    getAllMessagesAsObject = function() {
+      var obj, threads;
+      threads = getAllMessagesByGmailAPI.call(this);
+      obj = threads.map((function(_this) {
+        return function(t) {
+          var messages;
+          messages = t.messages.map(function(e) {
+            var bodyObj, o, payload;
+            o = {};
+            o.id = e.id;
+            payload = e.payload;
+            payload.headers.forEach(function(h) {
+              switch (h.name.toUpperCase()) {
+                case "DATE":
+                  o.date = new Date(h.value);
+                  break;
+                case "FROM":
+                  o.from = h.value;
+                  break;
+                case "TO":
+                  o.to = h.value;
+                  break;
+                case "SUBJECT":
+                  o.subject = h.value;
+              }
+              if (!o.date && "internalDate" in e) {
+                return o.date = new Date(Number(e.internalDate));
+              }
+            });
+            bodyObj = getData.call(_this, payload, _this.obj.exportAsObject, _this.cellLmit);
+            _this.obj.statistics.totalMessageSize += bodyObj.totalMessageSize;
+            _this.obj.statistics.totalAttachmentFiles += bodyObj.totalAttachmentFiles;
+            _this.obj.statistics.totalAttachmentFileSize += bodyObj.totalAttachmentFileSize;
+            o.body = bodyObj.bodies;
+            return o;
+          });
+          return {
+            threadId: t.id,
+            messages: messages
+          };
+        };
+      })(this));
+      obj.reverse();
+      this.obj.statistics.totalThreads = threads.length;
+      return obj;
+    };
+
+    getAllMessagesByGmailAPI = function() {
+      var ids, lo, pageToken, res, resource, threadIds, threads, userId;
+      userId = this.obj.userId;
+      if ("labelNames" in this.obj && Array.isArray(this.obj.labelNames)) {
+        lo = Gmail.Users.Labels.list(userId).labels.reduce(function(o, e) {
+          o[e.name] = e.id;
+          return o;
+        }, {});
+        this.obj.labelIds = this.obj.labelNames.map(function(e) {
+          return lo[e];
+        });
+      }
+      threadIds = [];
+      pageToken = "";
+      resource = {
+        maxResults: 1000
+      };
+      if ("labelIds" in this.obj && this.obj.labelIds.length > 0) {
+        resource.labelIds = this.obj.labelIds;
+      }
+      while (true) {
+        resource.pageToken = pageToken;
+        res = Gmail.Users.Threads.list(userId, resource);
+        ids = res.threads.map(function(e) {
+          return e.id;
+        });
+        Array.prototype.push.apply(threadIds, ids);
+        pageToken = res.nextPageToken;
+        if (!pageToken) {
+          break;
+        }
+      }
+      threads = getMessages.call(this, userId, threadIds);
+      return threads;
+    };
+
+    getMessages = function(userId, threadIds) {
+      var batchReqs, i, j, limit, ref, requests, response, split, temp, threads, threadsFromBatch, url;
+      limit = 100;
+      split = Math.ceil(threadIds.length / limit);
+      threads = [];
+      url = this.url;
+      for (i = j = 0, ref = split; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+        batchReqs = threadIds.splice(0, limit).map(function(id) {
+          return {
+            method: "GET",
+            endpoint: url + userId + "/threads/" + id + "?fields=*"
+          };
+        });
+        requests = {
+          batchPath: "/gmail/v1/users/",
+          requests: batchReqs
+        };
+        response = BatchRequest.Do(requests).getContentText();
+        temp = response.split("--batch");
+        threadsFromBatch = temp.slice(1, temp.length - 1).map(function(e) {
+          return JSON.parse(e.match(/{[\S\s]+}/g)[0]);
+        });
+        Array.prototype.push.apply(threads, threadsFromBatch);
+      }
+      return threads;
+    };
+
+    getData = function(ar, exportAsObject, cellLmit) {
+      return (function() {
+        var c;
+        return (c = function(ar, res) {
+          var body, bodySize, fileSize;
+          if ("parts" in ar && Array.isArray(ar.parts)) {
+            ar.parts.forEach(function(e) {
+              c(e, res);
+            });
+          }
+          if ("data" in ar.body) {
+            body = Utilities.newBlob(Utilities.base64DecodeWebSafe(ar.body.data)).getDataAsString();
+            if (!exportAsObject && body.length > cellLmit) {
+              body = body.slice(0, cellLmit);
+            }
+            bodySize = ar.body.size;
+            res.totalMessageSize += bodySize;
+            res.bodies.push({
+              mimeType: ar.mimeType,
+              size: bodySize,
+              body: body
+            });
+          } else if ("attachmentId" in ar.body) {
+            fileSize = ar.body.size;
+            res.totalAttachmentFileSize += fileSize;
+            res.totalAttachmentFiles += 1;
+            res.bodies.push({
+              mimeType: ar.mimeType,
+              size: fileSize,
+              filename: ar.filename,
+              attachmentId: ar.body.attachmentId
+            });
+          }
+          return res;
+        })(ar, {
+          bodies: [],
+          totalMessageSize: 0,
+          totalAttachmentFiles: 0,
+          totalAttachmentFileSize: 0
+        });
+      })();
+    };
+
+    convertObjectToArray = function(obj) {
+      var maxLen, values;
+      values = obj.reduce(function(oa, e) {
+        var temp;
+        temp = e.messages.map(function(m) {
+          var ar, body;
+          ar = [e.threadId, m.date, m.id, m.from, m.to, m.subject];
+          body = m.body.reduce(function(ma, b) {
+            t;
+            var t;
+            if (Array.isArray(b)) {
+              t = b.reduce(function(ba, bb) {
+                Array.prototype.push.apply(ba, [bb.mimeType, bb.size, bb.body]);
+                return ba;
+              }, []);
+            } else {
+              t = [b.mimeType, b.size];
+              if ("filename" in b) {
+                t.push(b.filename);
+              }
+              if ("attachmentId" in b) {
+                t.push(b.attachmentId);
+              }
+              if ("body" in b) {
+                t.push(b.body);
+              }
+            }
+            Array.prototype.push.apply(ma, t);
+            return ma;
+          }, []);
+          Array.prototype.push.apply(ar, body);
+          return ar;
+        });
+        Array.prototype.push.apply(oa, temp);
+        return oa;
+      }, []);
+      if (this.obj.headers) {
+        values.unshift(["threadId", "date", "messageId", "from", "to", "subject", "body including information of attachment files"]);
+      }
+      maxLen = Math.max.apply(null, values.map(function(e) {
+        return e.length;
+      }));
+      values = values.map(function(e) {
+        Array.prototype.push.apply(e, Array.apply(null, new Array(maxLen - e.length)).map(function(_, i) {
+          return "";
+        }));
+        return e;
+      });
+      this.obj.statistics.totalMessages = values.length;
+      return values;
+    };
+
+    return GmailToList;
+
+  })();
+  return r.GmailToList = GmailToList;
+})(this);
---
+++ README.md
@@ -0,0 +1,146 @@
+# GmailToList
+
+<a name="top"></a>
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENCE)
+
+<a name="overview"></a>
+
+# Overview
+
+**This is a library for exporting all messages of Gmail as a list using Google Apps Script (GAS).**
+
+<a name="description"></a>
+
+# Description
+
+Recently, I have had a situation it had been required to backup all messages in own Gmail. In order to achieve this, I created a simple script. After I created it, I thought that when such situation might occur for other users and the script is published as a library, they might be useful. So I created this library. But I created this for my situation. So if this cannot be used for your environment and an error occurs, I apologize.
+
+When this library is used, all messages of own Gmail can be exported as a list. This library can export a list as an object and an array. The array can be directly used for putting it to Google Spreadsheet.
+
+# Library's project key
+
+```
+1ZZfjzKy37lzld3fIB9N3EQ3I1A6Mixz2MWqpu5f3YoiHUMAdCX0lEMYh
+```
+
+<a name="usage"></a>
+
+# Usage:
+
+## 1. Install library
+
+In order to use this library, please install this library, enable Gmail API at Advanced Google services. You can see the flow of them as follows.
+
+1. Create a GAS project.
+
+   - You can use this library for the GAS project of both the standalone type and the container-bound script type.
+
+1. [Install GmailToList library](https://developers.google.com/apps-script/guides/libraries).
+
+   - Library's project key is **`1ZZfjzKy37lzld3fIB9N3EQ3I1A6Mixz2MWqpu5f3YoiHUMAdCX0lEMYh`**.
+
+1. [Enable Gmail API at Advanced Google services](https://developers.google.com/apps-script/guides/services/advanced#enabling_advanced_services).
+
+### About scopes
+
+About the install of scopes used at this library, users are not required to install scopes. Because this library can automatically install the required scopes to the project which installed this library. The detail information about this can be seen at [here](https://gist.github.com/tanaikech/23ddf599a4155b66f1029978bba8153b).
+
+The following scopes are automatically set when you install this library. When you run the script for the 1st time, please authorize them.
+
+- `https://www.googleapis.com/auth/gmail.readonly`
+- `https://www.googleapis.com/auth/script.external_request`
+
+### External library
+
+This library uses an external GAS library. It's [BatchRequest](https://github.com/tanaikech/BatchRequest). All messages of Gmail are retrieved by the batch request. In this case, the number of API call can be reduced. But the current quota of "Email read/write (excluding send)" is 20,000 / day. [Ref](https://developers.google.com/apps-script/guides/services/quotas) Please be careful this.
+
+## 2. Sample script
+
+### Sample 1
+
+A sample script is as follows.
+
+```javascript
+var res = GmailToList.Do();
+
+Logger.log(res);
+// Logger.log(res.array);
+
+//  SpreadsheetApp.getActiveSheet().getRange(1, 1, res.array.length, res.array[0].length).setValues(res.array);
+```
+
+- In this sample script, the object like `res = {array: [], statistics: {}}` is returned. `res.array` and `res.statistics` are the 2 dimensional array for putting to Google Spreadsheet and the statistics of Gmail, respectively.
+  - The statistics are the total threads, total messages, total message size, total attachment files and total attachment file size.
+- In this script, when `SpreadsheetApp.getActiveSheet().getRange(1, 1, res.array.length, res.array[0].length).setValues(res.array);` is used, the result value can be put to the active sheet.
+
+### Sample 2
+
+```javascript
+var object = {
+  userId: "me",
+  labelNames: ["sample1", "sample2"],
+  exportAsObject: true
+  //   headers: false
+};
+var res = GmailToList.Do(object);
+
+Logger.log(res);
+// Logger.log(res.object);
+```
+
+- `userId`: Default value is `me`.
+- `labelNames`: Default value is `null`. If you want to retrieve the messages with the label names, please set this. At above sample script, the messages with the label names of `sample1 AND sample2` are retrieved.
+- `exportAsObject`: Default is `false`. When you use this as `true`, the result is returned as an object. In this case, you can see it at `res.object`.
+  - When you use this as `false`, the result is returned as an array. In this case, you can see it at `res.array`. And this can be directly used for putting the Google Spreadsheet. So the values for each cell are rounded for 50,000 bytes which is the maximum length of one cell.
+- `headers`: Default is `true`. This is used for `exportAsObject: false`. When this is used as `false`, the headers for the returned array are not included.
+
+### Sample result
+
+The value of `res.statistics` is as follows.
+
+```json
+{
+  "totalThreads": #,
+  "totalMessages": #,
+  "totalMessageSize": #,
+  "totalAttachmentFiles": #,
+  "totalAttachmentFileSize": #
+}
+```
+
+The value of `res.array` is as follows.
+
+```
+[
+  ["threadId","date","messageId","from","to","subject","body including in formation of attachment files","","","","","","","","","","","","","","","","","",],
+  ["###threadId###","2000-01-01T00:00:00.000Z","###messageId###","###from###","###to###","###subject###","text/plain","###size###","###textbody###","text/html","###size###","###HTMLbody###","###mimeTypeofattachmentfile###","###size###","###filename###","###attachmentId###","###mimeTypeofattachmentfile###","###size###","###filename###","###attachmentId###","###mimeTypeofattachmentfile###","###size###","###filename###","###attachmentId###"]
+]
+```
+
+- `attachmentId`: When present, contains the ID of an external attachment that can be retrieved in a separate messages.attachments.get request. When not present, the entire content of the message part body is contained in the data field. [Ref](https://developers.google.com/gmail/api/v1/reference/users/messages/attachments)i/v1/reference/users/messages/attachments/get
+
+---
+
+<a name="licence"></a>
+
+# Licence
+
+[MIT](LICENCE)
+
+<a name="author"></a>
+
+# Author
+
+[Tanaike](https://tanaikech.github.io/about/)
+
+If you have any questions and commissions for me, feel free to tell me.
+
+<a name="updatehistory"></a>
+
+# Update History
+
+- v1.0.0 (December 16, 2019)
+
+  1. Initial release.
+
+[TOP](#top)
